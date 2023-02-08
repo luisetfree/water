be3837a7c6b1603aff06f7ddf8ebb8c189167113
luisetfree <luisetfree@gmail.com>
2023-02-08 08:57:24 -0600

    En barra de progreso se agregó el dato de la produccion diaria como un mensaje emergente, se podrá ver al poner el mouse sobre la barra

--- resources/views/resumen.blade.php
+++ resources/views/resumen.blade.php
@@ -544,7 +544,7 @@
 <div class="progress">
     
     
-   <div class="progress-bar progress-bar-striped progress-bar-animated" style="width : {{$progreso}}%">Produccion diaria  {{$progreso}}%</div>
+   <div title="Producción Bocatoma: {{$caudal_bt}} m³ " class="progress-bar progress-bar-striped progress-bar-animated" style="width : {{$progreso}}%">Producción {{$progreso}}%</div>
 
     
 </div>
